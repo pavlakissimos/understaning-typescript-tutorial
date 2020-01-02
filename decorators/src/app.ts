61ba837d37c6fd80e9ed6217d7f38fe969b21710
spavlakis <you@example.comspavlakis.modulus.gr>
2020-01-02 20:32:51 +0200

    Accessor & Parameter decorators

--- decorators/src/app.ts
+++ decorators/src/app.ts
@@ -7,6 +7,7 @@ function Logger(logString: string) {
 
 function WithTemplate(template: string, hookId: string) {
   return function(constructor: any) {
+    console.log("Rendering template");
     const hookEl = document.getElementById(hookId);
     const pers = new constructor();
     if (hookEl) {
@@ -16,6 +17,7 @@ function WithTemplate(template: string, hookId: string) {
   };
 }
 // @Logger("LOGGING - PERSON")
+@Logger("LOGGING")
 @WithTemplate("<h1>My Person Object</h1>", "app")
 class Person {
   name = "Simos";
@@ -28,3 +30,60 @@ class Person {
 const person = new Person();
 
 console.log(person);
+
+//  ---
+
+function Log(target: any, propertyName: string) {
+  console.log("Property decorator!");
+  console.log(target, propertyName);
+}
+
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+  console.log("Accessor decorator!");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+function Log3(
+  target: any,
+  name: string | symbol,
+  descriptor: PropertyDescriptor
+) {
+  console.log("Method decorator!");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+function Log4(target: any, name: string | symbol, position: number) {
+  console.log("Parameter decorator!");
+  console.log(target);
+  console.log(name);
+  console.log(position);
+}
+
+class Product {
+  @Log
+  title: string;
+  private _price: number;
+
+  @Log2
+  set price(val: number) {
+    if (val) {
+      this._price = val;
+    } else {
+      throw new Error("Invalid price - should be positive!");
+    }
+  }
+
+  constructor(title: string, price: number) {
+    this.title = title;
+    this._price = price;
+  }
+
+  @Log3
+  getPriceWithTax(@Log4 tax: number) {
+    return this._price * (1 + tax);
+  }
+}
