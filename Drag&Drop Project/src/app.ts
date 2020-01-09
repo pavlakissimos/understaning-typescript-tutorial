1707a8f642faa065895d03e5506d6ebb98efd894
spavlakis <you@example.comspavlakis.modulus.gr>
2020-01-09 17:22:46 +0200

    Create autobind decorator

--- Drag&Drop Project/src/app.ts
+++ Drag&Drop Project/src/app.ts
@@ -1,74 +1,85 @@
-// Show form
+// Autobind decorator
+function Autobind(_target: any, _methodName: string | Symbol, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjustedDescriptor: PropertyDescriptor = {
+    configurable: true,
+    enumerable: false,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    }
+  };
 
-// const app = document.getElementById("app")!;
-// const projectInput = document.getElementById("project-input") as HTMLTemplateElement;
+  return adjustedDescriptor;
+}
 
-// const projectInputClone = document.importNode(projectInput.content, true);
-
-// app.appendChild(projectInputClone);
-
+// Project input class
 class ProjectInput {
   templateEl: HTMLTemplateElement;
   hostEl: HTMLDivElement;
   element: HTMLFormElement;
+  titleEl: HTMLInputElement;
+  descriptionEl: HTMLTextAreaElement;
+  peopleEl: HTMLInputElement;
 
   constructor() {
     this.templateEl = document.getElementById("project-input")! as HTMLTemplateElement;
     this.hostEl = document.getElementById("app")! as HTMLDivElement;
 
-    const projectInputClone = document.importNode(this.templateEl.content, true);
-    this.element = projectInputClone.firstElementChild as HTMLFormElement;
+    const importedNode = document.importNode(this.templateEl.content, true);
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = "user-input";
 
+    this.titleEl = this.element.querySelector("#title") as HTMLInputElement;
+    this.descriptionEl = this.element.querySelector("#description") as HTMLTextAreaElement;
+    this.peopleEl = this.element.querySelector("#people") as HTMLInputElement;
+
+    this.configure();
     this.attach();
   }
 
   private attach() {
     this.hostEl.insertAdjacentElement("afterbegin", this.element);
   }
+
+  private gatherUserInput(): [string, string, number] | void {
+    const title = this.titleEl.value,
+      description = this.descriptionEl.value,
+      people = +this.peopleEl.value;
+
+    if (!title.length && !description.length && people > 0) {
+      alert("Invalid input, please try again!");
+    } else {
+      return [title, description, people];
+    }
+  }
+
+  @Autobind
+  private submitHandler(e: Event) {
+    e.preventDefault();
+    const userInput = this.gatherUserInput();
+
+    if (Array.isArray(userInput)) {
+      const [title, description, people] = userInput;
+      console.log(title, description, people);
+    }
+  }
+
+  @Autobind
+  private configure() {
+    this.element.addEventListener("submit", this.submitHandler);
+  }
 }
 
 const projectInput = new ProjectInput();
 
 // Create project
-
-interface ValidatorConfig {
-  [property: string]: {
-    [validatableProp: string]: string[]; // ['required', 'positive]
-  };
-}
-
-const registeredValidators: ValidatorConfig = {};
-
-function TruthyValue(target: any, propName: string) {
-  registeredValidators[target.constructor.name] = {
-    ...registeredValidators[target.constructor.name],
-    [propName]: [...registeredValidators[target.constructor.name][propName], "required"]
-  };
-}
-
-function isValid(obj: any) {
-  const objValidatorConfig = registeredValidators[obj.constructor.name];
-  let isValid = true;
-
-  for (const prop in objValidatorConfig) {
-    for (const validator of objValidatorConfig[prop]) {
-      switch (validator) {
-        case "required":
-          isValid = isValid && !!obj[prop];
-          break;
-      }
-    }
-  }
-
-  return isValid;
-}
-
 class Project {
-  @TruthyValue
+  // @TruthyValue
   title!: string;
-  @TruthyValue
+  // @TruthyValue
   description!: string;
-  @TruthyValue
+  // @TruthyValue
   people!: number;
 
   constructor(title: string, description: string, people: number) {
@@ -76,29 +87,4 @@ class Project {
     this.description = description;
     this.people = people;
   }
-
-  addProject() {
-    console.log(this.title, this.description, this.people);
-  }
 }
-
-// Form
-
-const form = document.querySelector("form")!;
-
-form.addEventListener("submit", e => {
-  e.preventDefault();
-  const titleEl = document.getElementById("title") as HTMLInputElement;
-  const descriptionEl = document.getElementById("description") as HTMLInputElement;
-  const peopleEl = document.getElementById("people") as HTMLInputElement;
-  const title = titleEl.value;
-  const description = descriptionEl.value;
-  const people = +peopleEl.value;
-  const project = new Project(title, description, people);
-
-  if (isValid(project)) {
-    project.addProject();
-  } else {
-    alert("Not valid values!");
-  }
-});
